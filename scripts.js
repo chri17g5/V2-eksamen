96f92fce645888cce249a9ad38f36d636c78382c
Christian Weirum <64000252+CyberBute@users.noreply.github.com>
2021-03-10 15:00:01 +0100

    Finished i guess
    not quite done like  would love to but done is done

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 "use strict";
 
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } 
+  else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+
+  prevScrollpos = currentScrollPos;
+}
